83afedceb9e24d4cec4cc9d88be3d059d2b0a3c6
Justalittlehusky <Tylerwbridges@gmail.com>
2016-08-13 13:12:31 -0700

    I had a pretty had time getting the hadouken to stop and reset. Following the projects example I was able to get through it but I don't fully understand it.

--- js/app.js
+++ js/app.js
@@ -19,5 +19,39 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
-	});
+	})
+
+	.mousedown(function() {
+		console.log('Mouse down')
+			playHadouken()
+			$('.ryu-ready').hide();
+			$('.ryu-throwing').show();
+			$('.hadouken').finish().show().animate(
+        {'left': '300px'},
+        500,
+        function() {
+          $('.hadouken').stop();
+          $('.hadouken').hide();
+          $('.hadouken').css('left', '-212px');
+        }
+      );
+
+	// Ryu drops and fires hadouken
+	//hadouken noise plays	
+	})
+
+	.mouseup(function() {
+		console.log("Mouse up")
+			$('.ryu-throwing').hide();
+			$('.hadouken').hide();
+			$('.ryu-ready').show();
+	//Ryu returns to ready stance
+	})
+
 });
+
+function playHadouken(){
+	$('#playHadouken')[0].volume = 0.5;
+	$('#playHadouken')[0].load();
+	$('#playHadouken')[0].play();
+};
